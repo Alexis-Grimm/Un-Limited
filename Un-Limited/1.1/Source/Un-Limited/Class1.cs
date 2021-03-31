569e92999a00e577b8647e5a3fa38433173b192e
Alexis-Grimm <ropernigel0@gmail.com>
2021-03-31 02:17:39 -0400

    created solution for c# testing
    Beginning to test out an assembly for the purpose of mod options, created the testing branch to get started

---
+++ Un-Limited/1.1/Source/Un-Limited/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace Grimm_UnLimited
+{
+    [StaticConstructorOnStartup]
+    public static class Grimm_UnLimited
+    {
+        static Grimm_UnLimited()
+		{
+            Log.Message("Hello World!");
+		}
+    }
+}
